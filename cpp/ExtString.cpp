ad4cfbbe9bd61c0111749be307a6163920349b51
= <aillieo@163.com>
2017-04-20 18:41:33 +0800

    update

--- cpp/ExtString.cpp
+++ cpp/ExtString.cpp
@@ -1,16 +1,29 @@
 #include "ExtString.h"
 #include <string>
+#include <stdarg.h>
+#include <codecvt>
 
 using std::string;
+using std::wstring;
 
 void ExtString::reverseString(string& str)
 {
 	reverse(str.begin(), str.end());
 }
 
-std::string ExtString::getReversedString(const string& str)
+void ExtString::reverseString(wstring& str)
+{
+	reverse(str.begin(), str.end());
+}
+
+string ExtString::getReversedString(const string& str)
 {
 	return string(str.rbegin(), str.rend());
+}
+
+wstring ExtString::getReversedString(const wstring& str)
+{
+	return wstring(str.rbegin(), str.rend());
 }
 
 char* ExtString::reverseCString( char* cstr )
@@ -26,4 +39,174 @@ char* ExtString::reverseCString( char* cstr )
 	}
 	return cstr;
 }
+
+string ExtString::format( const char * format, ... )
+{
+	char* pBuf = (char*)malloc(MaxStringLen);
+	if(pBuf == nullptr)
+	{
+		return "";
+	}
+	else
+	{
+		va_list arglist;
+		va_start(arglist, format);
+		vsnprintf(pBuf, MaxStringLen, format, arglist);
+		va_end(arglist);
+		string tmp = string(pBuf);
+		free(pBuf);
+		return tmp;
+	}
+}
+
+vector<string> ExtString::splitString( const string& str , const string& sep )
+{
+	vector<string> strs;
+	if (str.empty())
+	{
+		return strs;
+	}
+
+	string tmp;
+	string::size_type pos_begin = str.find_first_not_of(sep);
+	string::size_type comma_pos = 0;
+
+	while (pos_begin != string::npos)
+	{
+		comma_pos = str.find(sep, pos_begin);
+		if (comma_pos != string::npos)
+		{
+			tmp = str.substr(pos_begin, comma_pos - pos_begin);
+			pos_begin = comma_pos + sep.length();
+		}
+		else
+		{
+			tmp = str.substr(pos_begin);
+			pos_begin = comma_pos;
+		}
+
+		if (!tmp.empty())
+		{
+			strs.push_back(tmp);
+			tmp.clear();
+		}
+	}
+	return strs;
+}
+
+string ExtString::trim( const string& str, bool trimLeft/*=true*/, bool trimRight/*=true*/ )
+{
+	if(!trimLeft && !trimRight){
+		return str;
+	}
+
+	string tmp = str;
+	string::iterator it;
+
+	if(trimLeft)
+	{
+		for (it = tmp.begin(); it != tmp.end(); it++) 
+		{
+			if (!isspace(*it)) 
+			{
+				tmp.erase(tmp.begin(), it);
+				break;
+			}
+		}
+
+		if (it == tmp.end()) 
+		{
+			return tmp;
+		}
+	}
+
+	if (trimRight)
+	{
+		for (it = tmp.end() - 1; it != tmp.begin(); it--) 
+		{
+			if (!isspace(*it)) 
+			{
+				tmp.erase(it + 1, tmp.end());
+				break;
+			}
+		}
+	}
+
+	return tmp;
+}
+
+/*
+wstring ExtString::string2wstring( const string& str )
+{
+	typedef std::codecvt_utf8<wchar_t> convert_typeX;
+	std::wstring_convert<convert_typeX, wchar_t> converterX;
+	return converterX.from_bytes(str);
+}
+
+string ExtString::wstring2string( const wstring& str )
+{
+	typedef std::codecvt_utf8<wchar_t> convert_typeX;
+	std::wstring_convert<convert_typeX, wchar_t> converterX;
+	return converterX.to_bytes(str);
+}
+*/
+
+vector<string> ExtString::file2vector( const char* filepath )
+{
+	vector<string> ret = vector<string>();
+	if(NULL == filepath)
+	{
+		return ret;
+	}
+
+	// read file to buffer
+
+	unsigned char* pBuffer = NULL;
+	unsigned long bufferSize = 0;
+	do
+	{
+		FILE *fp = fopen(filepath, "r");
+		if(!fp)
+		{
+			return ret;
+		}
+		fseek(fp,0,SEEK_END);
+		bufferSize = ftell(fp);
+		fseek(fp,0,SEEK_SET);
+		pBuffer = new unsigned char[bufferSize];
+		bufferSize = fread(pBuffer,sizeof(unsigned char), bufferSize,fp);
+		fclose(fp);
+	} while (0);
+
+	if (!pBuffer)
+	{
+		return ret;
+	}
+
+	// move from buffer to vector
+
+	int lineLen = 0;
+	string lineStr = ""; 
+	for (unsigned long i = 0; i < bufferSize; i++)
+	{
+		if (pBuffer[i] != '\r' && pBuffer[i] != '\n' && pBuffer[i] != '\0')
+		{
+			lineStr += pBuffer[i];
+			lineLen++;
+		}
+		else
+		{
+			if (lineLen > 0)
+			{
+				std::string oneLineStr(lineStr.data(), lineStr.length());
+				ret.push_back(oneLineStr);
+				lineStr = "";
+				lineLen = 0 ;
+			}
+		}
+	}
+	delete[] pBuffer;
+	return ret;
+}
+
 
--- cpp/ExtString.h
+++ cpp/ExtString.h
@@ -3,14 +3,40 @@
 
 #pragma once
 
+#define MaxStringLen 1024
+
 using std::string;
+using std::wstring;
+using std::vector;
 
 namespace ExtString 
 {
+	// 反转字符串
 	void reverseString(string& str);
+	void reverseString(wstring& str);
 
-	std::string getReversedString(const string& str);
+	// 反转字符串
+	string getReversedString(const string& str);
+	wstring getReversedString(const wstring& str);
 
+	// 反转字符串
 	char* reverseCString(char* cstr);
+
+	// 格式化构造字符串
+	string format (const char * format, ...);
 	
+	// 分裂字符串
+	vector<string> splitString( const string& str , const string& sep );
+
+	// 修剪字符串
+	string trim(const string& str, bool trimLeft=true, bool trimRight=true);
+
+	// string 和 wstring互转
+	//wstring string2wstring(const string& str);
+	//string wstring2string(const wstring& str);
+
+	// 读取文件 每行一个string
+	vector<string> file2vector(const char* filepath);
+
+
 }
