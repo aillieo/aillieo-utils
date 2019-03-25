9745948d253aa968b60facf12de6927f3c7a458d
aillieo <aillieo@163.com>
2019-03-25 21:24:35 +0800

    update

---
+++ unity/Script/Misc/SerializeHelper.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
+namespace AillieoUtils
+{
+    public static class SerializeHelper
+    {
+
+        public static bool DeserializeBytesToData<T>(string filename, out T obj)
+        {
+            if(!FileUtils.FileExists(filename))
+            {
+                obj = default(T);
+                return false;
+            }
+            IFormatter formatter = new BinaryFormatter();
+            Stream stream = new FileStream(filename, FileMode.Open, FileAccess.Read, FileShare.Read);
+            obj = (T)formatter.Deserialize(stream);
+            stream.Close();
+            return true;
+        }
+
+
+        public static bool SerializeDataToBytes<T>(T obj, string filename)
+        {
+            IFormatter formatter = new BinaryFormatter();
+            Stream stream = new FileStream(filename, FileMode.Create, FileAccess.Write, FileShare.None);
+            formatter.Serialize(stream, obj);
+            stream.Close();
+            return true;
+        }
+
+
+        public static byte[] SerializeDataToBytes<T>(T obj)
+        {
+            MemoryStream stream = new MemoryStream();
+            IFormatter formatter = new BinaryFormatter();
+            formatter.Serialize(stream, obj);
+            byte[] byteArray = stream.ToArray();
+            stream.Close();
+            return byteArray;
+        }
+
+
+        public static T DeserializeBytesToData<T>(byte[] byteArray)
+        {
+            MemoryStream stream = new MemoryStream(byteArray);
+            IFormatter formatter = new BinaryFormatter();
+            T t = (T)formatter.Deserialize(stream);
+            stream.Close();
+            return t;
+        }
+    }
+
+}
---
+++ unity/Script/Misc/UIUtils.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AillieoUtils
+{
+    public static class UIUtils
+    {
+
+        public static void StrentchRectTransform(RectTransform rectTransform)
+        {
+            rectTransform.SetInsetAndSizeFromParentEdge(RectTransform.Edge.Left, 0, 0);
+            rectTransform.SetInsetAndSizeFromParentEdge(RectTransform.Edge.Top, 0, 0);
+            rectTransform.SetInsetAndSizeFromParentEdge(RectTransform.Edge.Right, 0, 0);
+            rectTransform.SetInsetAndSizeFromParentEdge(RectTransform.Edge.Bottom, 0, 0);
+            rectTransform.anchorMin = Vector2.zero;
+            rectTransform.anchorMax = Vector2.one;
+        }
+
+    }
+}
+
