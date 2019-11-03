cbf700d6834993514d28ead6f1f82d5697bd54ec
aillieo <aillieo@163.com>
2019-11-03 21:50:48 +0800

    update

--- unity/Script/ClassExt/TransformExt.cs
+++ unity/Script/ClassExt/TransformExt.cs
@@ -14,5 +14,26 @@ namespace AillieoUtils
             }
         }
 
+        public static Transform FindRecursive(this Transform transform, string name)
+        {
+            foreach (Transform t in transform)
+            {
+                if (t.name == name)
+                {
+                    return t;
+                }
+                else
+                {
+                    Transform result = t.FindRecursive(name);
+                    if (result != null)
+                    {
+                        return result;
+                    }
+                }
+            }
+
+            return null;
+        }
+
     }
 }
